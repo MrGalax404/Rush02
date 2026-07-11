4370fcc6def766fda6ea1b2ed58034cbe5a2c2e9
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-11 14:44:22 +0200

    fix struct file

---
+++ dict.h
@@ -0,0 +1,10 @@
+#ifndef DICT_H
+# define DICT_H
+
+typedef struct s_dict
+{
+	char *key;
+	char *value;
+}	t_dict;
+
+#endif
--- ft_struct.h
+++
@@ -1,5 +0,0 @@
-typedef struct s_number
-{
-	char *index;
-	char *word;
-}	t_number;
