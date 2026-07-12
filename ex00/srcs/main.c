121ca9e13675d7cf86e9bb3c2f5bdb4581af4faa
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-12 16:48:42 +0200

    fix dealing with 2 arg

--- ex00/srcs/main.c
+++ ex00/srcs/main.c
@@ -23,22 +23,29 @@ int main(int argc, char **argv)
 {
 	char 	*nbr;
 	char	*content;
+	char	*dict_file;
 	t_dict	*dict;
 	int	len;
 	int	is_first;
 
 	is_first = 1;
-	if(argc == 2)
-	nbr = argv[1];
-	else if(argc == 3)
+	if (argc == 2)
+	{
+		dict_file = "numbers.dict";
+		nbr = argv[1];
+	}
+	else if (argc == 3)
+	{
+		dict_file = argv[1];
 		nbr = argv[2];
+	}
 	else
 	{
-		write(2, "Dict Error\n", 11);
-		return 1;
+		write(1, "Error\n", 6);
+		return (1);
 	}
 	len = ft_strlen(nbr);
-	content = ft_read_file("numbers.dict");
+	content = ft_read_file(dict_file);
 	if (content == 0)
 	{
 		write(1, "Dict Error\n", 11);
