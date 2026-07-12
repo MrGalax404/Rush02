1490372a1253ff55d0dbaf9d2d2286e556b98a83
Noah Mathys <nmathys@c1r1s4.42lausanne.ch>
2026-07-12 14:18:07 +0200

    Adding find_in_dict function.

--- ex00/srcs/dict.c
+++ ex00/srcs/dict.c
@@ -3,13 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   dict.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nmathys <nmathys@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 14:13:07 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/12 14:13:34 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/12 14:16:28 by nmathys          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush02.h"
 
 char	*find_in_dict(t_dict *tab, char *key);
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+char *find_in_dict(t_dict *tab, char *key)
+{
+	int	i;
+	i = 0;
+	while (tab[i].key != NULL)
+	{
+		if (ft_strcmp(tab[i].key, key) == 0)
+			return (tab[i].value);
+		i++;
+	}
+	return (NULL);
+}
